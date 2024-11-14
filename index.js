bffeeef12f43acb2a853c5f7a9f0e47cdf45d892
Kareem <149588278+MeKareem4@users.noreply.github.com>
2024-11-14 07:58:34 -0500

    done!

---
+++ index.js
@@ -0,0 +1,18 @@
+function downloadFile(fileNumber) {
+  // Replace with the actual file URLs
+  const fileUrls = {
+    1: 'MeKareem4/kareems-projects/TempCleaner.zip',
+    2: 'path/to/file2.ext',
+    3: 'path/to/file3.ext',
+    4: 'path/to/file4.ext',
+    5: 'path/to/file5.ext',
+    6: 'path/to/file6.ext'
+  };
+
+  const url = fileUrls[fileNumber];
+  if (url) {
+    window.location.href = url;
+  } else {
+    alert("File URL not specified for this button.");
+  }
+}
