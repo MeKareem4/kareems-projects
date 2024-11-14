de923534f74e05a34cc7da1eea1f57f52577d33d
Kareem <149588278+MeKareem4@users.noreply.github.com>
2024-11-14 11:41:11 -0500

    done!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function downloadFile(fileNumber) {
   const fileUrls = {
     1: 'files/TempCleaner.zip',
     2: 'files/ScreenResolution.zip',
-    3: 'path/to/file3.ext',
+    3: 'files/TextToJapanese.zip',
     4: 'path/to/file4.ext',
     5: 'path/to/file5.ext',
     6: 'path/to/file6.ext'
